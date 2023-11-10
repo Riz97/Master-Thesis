459d08bd9fb95a39bef6e6a92951a1759ad5a43c
Riccardo Caprile <93477707+Riz97@users.noreply.github.com>
2023-11-10 10:59:51 +0100

    Update Domain.cs

--- Assets/Scripts/Domain.cs
+++ Assets/Scripts/Domain.cs
@@ -40,7 +40,7 @@ namespace RoslynCSharp.Example
             ScriptProxy proxy = type.CreateInstance();
 
             // Create an instance of 'Example' using the overload constructor
-            proxy = type.CreateInstance();
+           // proxy = type.CreateInstance();
 
 
             // Call the method called 'ExampleMethod' and pass the string argument 'World'
@@ -54,54 +54,7 @@ namespace RoslynCSharp.Example
         }
     }
 
-    public class Ex14_InstanceMethods_Updated : MonoBehaviour
-    {
-        private ScriptDomain domain = null;
-        private const string sourceCode = @"
-        using UnityEngine;
-        class Example
-        {
-            void ExampleMethod(string input)
-            {
-                Debug.Log(""Hello "" + input);
-            }
-        }";
-
-        public AssemblyReferenceAsset netStandardRef;
-        public AssemblyReferenceAsset unityRef;
-
-        /// <summary>
-        /// Called by Unity.
-        /// </summary>
-        public void Start()
-        {
-            // Create domain
-            domain = ScriptDomain.CreateDomain("Example Domain");
-
-            // Compile and load code - Note that we use 'CompileAndLoadMainSource' which is the same as 'CompileAndLoadSource' but returns the main type in the compiled assembly
-            ScriptType type = domain.CompileAndLoadMainSource(sourceCode, ScriptSecurityMode.UseSettings, new Compiler.IMetadataReferenceProvider[]
-            {
-                netStandardRef,
-                unityRef,
-            });
-
-            // Create an instance of 'Example'
-            ScriptProxy proxy = type.CreateInstance();
-
-            // Create an instance of 'Example' using the overload constructor
-            proxy = type.CreateInstance();
-
-
-            // Call the method called 'ExampleMethod' and pass the string argument 'World'
-            // Note that any exceptions thrown by the target method will not be caught
-            proxy.Call("ExampleMethod", "World");
-
-
-            // Call the method called 'ExampleMethod' and pass the string argument 'Safe World'
-            // Note that any exceptions thrown by the target method will handled as indicated by the 'Safe' name
-            proxy.SafeCall("ExampleMethod", "Safe World");
-        }
-    }
+ 
 }
 
 
