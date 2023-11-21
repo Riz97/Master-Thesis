ac646e25de8668ace9df0c14b438366802a2aefd
Riccardo Caprile <93477707+Riz97@users.noreply.github.com>
2023-11-21 16:58:24 +0100

    .

---
+++ Assets/Scripts/Change_Scene.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Change_Scene : MonoBehaviour
+{
+    // Start is called before the first frame update
+   public void  Developer_Scene()
+    {
+        SceneManager.LoadScene("Developer_Scene");
+    }
+
+    public void User_Scene()
+    {
+        SceneManager.LoadScene("User_Scene");
+    }
+}
