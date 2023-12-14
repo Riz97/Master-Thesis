0546dc04f03da44d2df8d2add43b5059b1558e45
Riccardo Caprile <93477707+Riz97@users.noreply.github.com>
2023-12-14 14:17:32 +0100

    .

--- Assets/Scripts/Change_Scene.cs
+++ Assets/Scripts/Change_Scene.cs
@@ -32,6 +32,6 @@ public class Change_Scene : MonoBehaviour
     }
     public void VR_Developer_Scene()
     {
-        SceneManager.LoadScene("/VR Scenes/Developer_Scene");
+        SceneManager.LoadScene(5);
     }
 }
