faec8a0f9cadd68c28b3ed1b64b5f011e411cc6b
Riccardo Caprile <93477707+Riz97@users.noreply.github.com>
2023-12-19 11:16:25 +0100

    .

--- Assets/Scripts/Vocal_Commands.cs
+++ Assets/Scripts/Vocal_Commands.cs
@@ -1,18 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Vocal_Commands : MonoBehaviour
 {
+
+
+    [SerializeField]
+    private Button startButton;
+
+    [SerializeField]
+    private Button stopButton;
+
+    [SerializeField]
+    private TMP_Text text;
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
+        startButton.onClick.AddListener(StartRecording);
+        stopButton.onClick.AddListener(StopRecording);
         
+
     }
+
 
     // Update is called once per frame
     void Update()
     {
         
     }
+
+
+
+    private void StartRecording()
+    {
+
+    }
+
+    private void StopRecording()
+    {
+
+    }
+
+
 }
