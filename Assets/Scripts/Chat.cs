e2374d5dfb1d0dc2400658e57d4ee88382efb9f3
Riccardo Caprile <93477707+Riz97@users.noreply.github.com>
2024-05-14 18:15:50 +0200

    Update Chat.cs

--- Assets/Scripts/Chat.cs
+++ Assets/Scripts/Chat.cs
@@ -581,12 +581,13 @@ public class Chat : MonoBehaviour
             Text.color = new Color(255, 0, 0);
             Text.SetText("Error : you have to ask for the exactly amount of models requested  for this simulation");
             Generate_Script_Button.interactable = true;
-        
+            input = null; // in this way the execution is blocked
+
             if (sceneName == "VR_User_Scene" || sceneName == "User_Scene")
         {
                 Info_Text.text = ("Error : you have to ask for the exactly amount of models requested  for this simulation");
                 Generate_Script_Button.interactable = true;
-
+                input = null; // in this way the execution is blocked
 
             }
 
@@ -612,9 +613,9 @@ public class Chat : MonoBehaviour
                 Info_Text.text = ("Error : The environment you asked is not implemented yet, sorry");
                 Generate_Script_Button.interactable = true;
 
-                input = null;
+                input = null; //in this way the execution is blocked
             }
-            input = null; 
+            input = null; // in this way the execution is blocked
             //------------------------------------------------------------------------------------------------------------------
         }
 
